597cd90beaa2eb7b0c9abf718232766a1edc2bba
Ethan <ethanl22702@gmail.com>
2025-07-20 21:50:57 -0400

    Completed project

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-const CHOICE_ROCK = 0;
-const CHOICE_PAPER = 1;
-const CHOICE_SCISSORS = 2;
+let computerScore = 0;
+let humanScore = 0;
+
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random()*3);
+    const computerChoices = ['rock', 'paper', 'scissors'];
+    let computerChoice = Math.floor(Math.random()*3);
 
-    switch(choice) {
-        case CHOICE_ROCK:
-            return 'ROCK';
-        case CHOICE_PAPER:
-            return 'PAPER';
-        case CHOICE_SCISSORS:
-            return 'SCISSORS';
-    }
+    return computerChoices[computerChoice];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let choice = prompt('Enter your choice (rock, paper, or scissors)');
+    return choice.toLowerCase();
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`This round is a draw! Both players picked ${computerChoice}`);
+        return;
+    }
+    // Key choice beats value choice
+    const winningConfigurations = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper'
+    }
+
+    let computerWins = (winningConfigurations[computerChoice] == humanChoice);
+    let roundResultMessage;
+    if(computerWins) {
+        roundResultMessage = `You lose this round! ${computerChoice} beats ${humanChoice}`;
+        computerScore++;
+    } else {
+        roundResultMessage = `You win this round! ${humanChoice} beats ${computerChoice}`;
+        humanScore++;
+    }
+
+    console.log(roundResultMessage);
+}
+
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(computerChoice, humanChoice);
+    }
+
+    let gameResultMessage;
+    if(computerScore > humanScore) {
+        gameResultMessage = 'The computer wins the game :(';
+    } else if (humanScore > computerScore) {
+        gameResultMessage = 'You win the game :)';
+    } else {
+        gameResultMessage = 'The game is a draw :|';
+    }
+
+    console.log(gameResultMessage);
+}
+
+playGame();
